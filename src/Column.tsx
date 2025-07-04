9e0e03717d157e822479d00f6aa252651462c425
viradm334 <viradamayanti@student.telkomuniversity.ac.id>
2025-07-04 19:37:50 +0700

    first commit

---
+++ src/Column.tsx
@@ -0,0 +1,28 @@
+import { useDroppable } from '@dnd-kit/core';
+import TaskCard from './TaskCard';
+import type {Column as ColumnType} from './types';
+import type { Task } from './types';
+
+
+type ColumnProps = {
+    column: ColumnType;
+    tasks: Task[];
+};
+
+function Column({column, tasks}: ColumnProps){
+    const {setNodeRef} = useDroppable({
+        id: column.id
+    })
+    return(
+        <div className='flex w-80 flex-col rounded-lg bg-neutral-800 p-4'>
+            <h2 className='mb-4 semibold text-neutral-100'>{column.title}</h2>
+            <div ref={setNodeRef} className='flex flex-1 flex-col gap-4'>
+                {tasks.map(task => {
+                    return <TaskCard key={task.id} task={task}></TaskCard>
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Column
---
+++ src/TaskCard.tsx
@@ -0,0 +1,22 @@
+import { useDraggable } from "@dnd-kit/core";
+import type { Task } from "./types";
+
+type TaskCardProps = {
+    task: Task;
+}
+
+function TaskCard({task} : TaskCardProps){
+    const {attributes, listeners, setNodeRef, transform} = useDraggable({
+        id: task.id
+    });
+
+    const style = transform ? {transform: `translate(${transform.x}px, ${transform.y}px)`} : undefined;
+    return(
+        <div ref={setNodeRef} {...listeners} {...attributes} className="cursor-grab rounded-lg bg-neutral-700 p-4 shadow-sm hover:shadow-md" style={style}>
+            <h3 className="font-medium text-neutral-50">{task.title}</h3>
+            <p className="mt-2 text-sm text-neutral-400">{task.description}</p>
+        </div>
+    )
+}
+
+export default TaskCard
